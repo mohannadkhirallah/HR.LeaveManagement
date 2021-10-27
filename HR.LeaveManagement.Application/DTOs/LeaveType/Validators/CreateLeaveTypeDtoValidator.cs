765a3fa0f9e24689d866e56689cae60c48c9dc32
Mohannad Khirallah <mkhirallah@KloudBusiness.com>
2021-10-27 16:56:23 +0300

    Adding Fluent Validation

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator :AbstractValidator<CreateLeaveTypeDto>
+    {
+
+        public CreateLeaveTypeDtoValidator()
+        {
+            RuleFor(p => p.Name)
+                    .NotEmpty().WithMessage("{PropertyName} is required")
+                    .NotNull()
+                    .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 charachters.");
+
+            RuleFor(p => p.DefaultDays)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .NotNull()
+                .GreaterThan(0).WithMessage("{PropertyName} must be greater than zero")
+                .LessThan(100).WithMessage("{PropertyName} must be less than 100");
+
+        }
+    }
+}
